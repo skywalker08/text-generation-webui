701eab4686688a4fdb35408a44f33b2376c12cdb
muzaffer <muzaffertasiran@gmail.com>
2023-08-14 18:29:26 +0300

    bug fix

--- modules/html_generator.py
+++ modules/html_generator.py
@@ -39,12 +39,11 @@ def replace_blockquote(m):
 def translate_to_turkish(string):
   translator = Translator()
   translated_text = translator.translate(string, src='en', dest='tr')
-  return translated_text
+  return translated_text.text
 
 def convert_to_markdown(string):
     
     string = translate_to_turkish(string)
-    print(string)
     # Blockquote
     pattern = re.compile(r'\\begin{blockquote}(.*?)\\end{blockquote}', re.DOTALL)
     string = pattern.sub(replace_blockquote, string)
