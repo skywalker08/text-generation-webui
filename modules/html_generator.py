0e51f5f72608d2141f4db90a6a27a7ca29ca9d77
muzaffer <muzaffertasiran@gmail.com>
2023-08-15 10:28:04 +0300

    remove detect

--- modules/html_generator.py
+++ modules/html_generator.py
@@ -38,8 +38,8 @@ def replace_blockquote(m):
 
 def translate_to_turkish(string):
   translator = Translator()
-  if translator.detect(string).lang == 'tr':
-      return string
+  # if translator.detect(string).lang == 'tr':
+  #     return string
   
   translated_text = translator.translate(string, src='en', dest='tr')
   return translated_text.text
